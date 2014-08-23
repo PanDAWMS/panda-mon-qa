3a7b19468c290f4aba5b76f9dfb2c3b3192f0fd2
Jaroslava Schovancova <jschovan@gmail.com>
2014-08-23 00:18:16 -0400

    Save HTML page upon error.

--- pandamonqa/qasuite/suite.py
+++ pandamonqa/qasuite/suite.py
@@ -13,6 +13,7 @@ import urllib
 import urllib2
 import urlparse
 from BeautifulSoup import BeautifulSoup
+from datetime import datetime
 
 
 VERB_SILENT = 0
@@ -40,7 +41,8 @@ class QASuite(object):
 #    STATIC_PREFIX = ''
 #    STATICS = []
 #    STATIC_SERVER = ''
-
+    PAGE_CONTENT_DIRECTORY = ''
+    PAGE_URL_PREFIX = ''
 
     def configure(self, config_file):
         """
@@ -55,6 +57,8 @@ class QASuite(object):
         self.PAGE_ADDRESS = config.get('AddressInfo', 'page_address')
         self.PAGE_BROWSER = config.get('BrowserInfo', 'page_browser')
         self.PAGE_VERSION = config.get('VersionInfo', 'page_version')
+        self.PAGE_CONTENT_DIRECTORY = config.get('Debugging', 'page_content_directory')
+        self.PAGE_URL_PREFIX = config.get('Debugging', 'page_url_prefix')
         self.ALL_ANCHORS = []
         self.ALL_CLICKABLE_ANCHORS = []
 #        self.STATIC_LIST_ADDRESS = config.get('Statics', 'static_list_address')
@@ -66,6 +70,32 @@ class QASuite(object):
 #        printv(u'###### %s() OUT' % (inspect.stack()[0][3]), VERB_STANDARD)
 
         return config
+
+
+    def filenames(self):
+        filebasename = filename = fileurl = ''
+        try:
+            filebasename = '%s___%s%s' % (\
+                        str(self.PAGE_ADDRESS).replace('/', '%2F').replace(':', '%3A').replace('?', '%3F').replace('#', '%23').replace('=', '%3D'), \
+                        datetime.utcnow().strftime("%F-%H%M%S"), \
+                        '.html' \
+                        )
+            dir = self.PAGE_CONTENT_DIRECTORY
+            if not os.path.exists(dir):
+                dir = '/tmp/'
+            filename = '%s%s' % (\
+                        dir, \
+                        filebasename \
+                        )
+            fileurl = '%s%s' % (\
+                        self.PAGE_URL_PREFIX, \
+                        filebasename \
+                        )
+            print 'filename=', filename
+            print 'fileurl=', fileurl
+        except:
+            pass
+        return (filebasename, filename, fileurl)
 
 
     def check_version(self):
@@ -91,21 +121,35 @@ class QASuite(object):
                 result = "Page " + self.PAGE_ADDRESS + " timed out."
                 list_warnings.append((self.PAGE_ADDRESS, self.PAGE_VERSION, result))
             
-#        isOK = False
+        filebasename = filename = fileurl = ''
+        isOK = False
         try:
             twill.commands.code('200')
-#            isOK = True
+            isOK = True
         except twill.errors.TwillAssertionError:
             result = 'Page status code ' + self.PAGE_ADDRESS + ' was ' + str(twill.commands.browser.get_code()) + '.'
+            ### save the page when returned code is other than 200
+            try:
+                filebasename, filename, fileurl = self.filenames()
+                twill.commands.save_html(filename)
+            except:
+                pass
             #raise twill.errors.TwillAssertionError(result)
-            list_errors.append((self.PAGE_ADDRESS, self.PAGE_VERSION, result))
+            list_errors.append((self.PAGE_ADDRESS, self.PAGE_VERSION, result, fileurl))
 
-        try:
-            twill.commands.find(self.PAGE_VERSION)
-        except twill.errors.TwillAssertionError:
-            result = 'Expected string ' + self.PAGE_VERSION + ' which is not there.'
-#            raise twill.errors.TwillAssertionError(result)
-            list_errors.append((self.PAGE_ADDRESS, self.PAGE_VERSION, result))
+        if isOK:
+            ### find the version string
+            try:
+                twill.commands.find(self.PAGE_VERSION)
+            except twill.errors.TwillAssertionError:
+                result = 'Expected string ' + self.PAGE_VERSION + ' which is not there.'
+    #            raise twill.errors.TwillAssertionError(result)
+                try:
+                    filebasename, filename, fileurl = self.filenames()
+                    twill.commands.save_html(filename)
+                except:
+                    pass
+                list_errors.append((self.PAGE_ADDRESS, self.PAGE_VERSION, result, fileurl))
 
         if list_errors:
             printv('errors found: %s' % (list_errors))
@@ -113,4 +157,5 @@ class QASuite(object):
             printv('OK (%s)' % (self.PAGE_ADDRESS))
         printv(u'###### %s() OUT' % (inspect.stack()[0][3]), VERB_STANDARD)
         return (list_errors, list_warnings)
+
 
